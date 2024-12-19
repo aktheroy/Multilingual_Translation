27dc56e9a26dbe0293546386f9a64daffec41591
aktheroy <aktheroy@gmail.com>
2024-12-19 13:32:13 +0000

    "formated code across whole project"

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,6 @@
-import sys
-import os
 from flask import Flask, render_template, request, jsonify
 from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
-from flasgger import Swagger
+
 
 # Load the model and tokenizer
 def load_model():
@@ -12,30 +10,39 @@ def load_model():
     print(f"Tokenizer loaded: {tokenizer.__class__.__name__}")
     return model, tokenizer
 
+
 # Translate text using the model
 def translate_text(model, tokenizer, source_text, source_lang, target_lang):
     tokenizer.src_lang = source_lang
     encoded_text = tokenizer(source_text, return_tensors="pt")
-    generated_tokens = model.generate(**encoded_text, forced_bos_token_id=tokenizer.get_lang_id(target_lang))
-    translated_text = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    generated_tokens = model.generate(
+        **encoded_text, forced_bos_token_id=tokenizer.get_lang_id(target_lang)
+    )
+    translated_text = tokenizer.batch_decode(
+      generated_tokens, skip_special_tokens=True
+      )
     return translated_text[0]
 
-# Initialize Flask app
-app = Flask(__name__, template_folder='../Frontend/Templates', static_folder='../Frontend/static')
 
-# Configure Swagger
-swagger = Swagger(app)
+# Initialize Flask app
+app = Flask(
+    __name__,
+    template_folder="../Frontend/Templates",
+    static_folder="../Frontend/static",
+)
 
 # Load the model and tokenizer at startup
 model, tokenizer = load_model()
 
+
 # Route for the homepage
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # Route for translation
-@app.route('/translate', methods=['POST'])
+@app.route("/translate", methods=["POST"])
 def translate():
     """
     Translate text from one language to another.
@@ -72,14 +79,17 @@ def translate():
               example: "Bonjour, comment ça va ?"
     """
     data = request.get_json()
-    source_text = data.get('source_text')
-    source_lang = data.get('source_lang')
-    target_lang = data.get('target_lang')
+    source_text = data.get("source_text")
+    source_lang = data.get("source_lang")
+    target_lang = data.get("target_lang")
 
     # Translate the text using the model
-    translated_text = translate_text(model, tokenizer, source_text, source_lang, target_lang)
+    translated_text = translate_text(
+        model, tokenizer, source_text, source_lang, target_lang
+    )
 
-    return jsonify({'translated_text': translated_text})
+    return jsonify({"translated_text": translated_text})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
